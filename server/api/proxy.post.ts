37fa360b2ec118e51811ceea99cf02d0da484731
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2024-06-15 22:02:46 +0100

    fix: Added Chromium 'User-Agent' to proxy requests

--- server/api/proxy.post.ts
+++ server/api/proxy.post.ts
@@ -6,8 +6,9 @@ import { z } from "zod";
 function searchForManifest(html: string) {
   const $ = cheerio.load(html);
   const manifestURL = $("link[rel='manifest']").attr("href");
+
   return {
-    isFound: new Boolean(manifestURL) as boolean,
+    isFound: !!manifestURL,
   };
 }
 
@@ -21,18 +22,22 @@ export default defineEventHandler(async (event) => {
 
   const body = await readValidatedBody(event, urlSchema.parse);
 
-  const response = await $fetch<string>(body.url).catch(
-    async (error: FetchError) => {
-      switch (error.status) {
-        case 403:
-          const zenrows = new ZenRows(process.env.NUXT_ZENROWS_API_KEY ?? "");
-          const { data } = await zenrows.get(body.url);
-          return data as string;
-        case 404:
-          return null;
-      }
+  const response = await $fetch<string>(body.url, {
+    headers: {
+      "User-Agent":
+        "'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'",
     },
-  );
+  }).catch(async (error: FetchError) => {
+    console.log(error.status);
+    switch (error.status) {
+      case 403:
+        const zenrows = new ZenRows(process.env.NUXT_ZENROWS_API_KEY ?? "");
+        const { data } = await zenrows.get(body.url);
+        return data as string;
+      default:
+        return null;
+    }
+  });
 
   if (response) return searchForManifest(response);
 });
